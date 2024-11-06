55ec7f74192cd318dbfc53e5d350b652027a4678
RashedAbdullah <maarashed40@gmail.com>
2024-11-06 18:27:20 +0600

    trying to making for google ads running

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6921856465558245"
+          crossorigin="anonymous"
+        ></script>
+        <meta name="google-adsense-account" content="ca-pub-6921856465558245"></meta>
+      </head>
       <body className={`antialiased`}>
         <Analytics />
         <header className="bg-green-600 text-white shadow-md mb-4">
